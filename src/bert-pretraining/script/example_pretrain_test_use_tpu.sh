a75ae706f431d03d723101b82e0f03661cab6e5b
Jian Zhang <zhangjianthu@gmail.com>
2019-06-30 08:15:14 +0000

    sync code before generating tf record

--- src/bert-pretraining/script/example_pretrain_test_use_tpu.sh
+++ src/bert-pretraining/script/example_pretrain_test_use_tpu.sh
@@ -21,27 +21,27 @@ export BERT_BASE_DIR=~/bert-pretraining/data/bert/uncased_L-12_H-768_A-12
 #  --random_seed=12345 \
 #  --dupe_factor=5
 
-python ../third_party/bert/run_pretraining.py \
-  --input_file=gs://embeddings-data/tf_examples.tfrecord \
-  --output_dir=gs://embeddings-ckpt/pipeline-test  \
-  --do_train=True \
-  --do_eval=True \
-  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
-  --train_batch_size=32 \
-  --max_seq_length=128 \
-  --max_predictions_per_seq=20 \
-  --num_train_steps=20 \
-  --num_warmup_steps=10 \
-  --learning_rate=2e-5 \
-  --use_tpu=True \
-  --tpu_name=demo-tpu
-  # --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt
+#python ../third_party/bert/run_pretraining.py \
+#  --input_file=gs://embeddings-data/tf_examples.tfrecord \
+#  --output_dir=gs://embeddings-ckpt/pipeline-test  \
+#  --do_train=True \
+#  --do_eval=True \
+#  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+#  --train_batch_size=32 \
+#  --max_seq_length=128 \
+#  --max_predictions_per_seq=20 \
+#  --num_train_steps=20 \
+#  --num_warmup_steps=10 \
+#  --learning_rate=2e-5 \
+#  --use_tpu=True \
+#  --tpu_name=demo-tpu
+#  # --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt
 
-#export BERT_BASE_DIR_NEW=/tmp/pretraining_output
-#pytorch_pretrained_bert convert_tf_checkpoint_to_pytorch \
-#  $BERT_BASE_DIR_NEW/model.ckpt-20 \
-#  $BERT_BASE_DIR/bert_config.json \
-#  $BERT_BASE_DIR_NEW/pytorch_model.bin
+export BERT_BASE_DIR_NEW=gs://embeddings-ckpt/pipeline-test
+python3 ../third_party/pytorch-pretrained-BERT/pytorch_pretrained_bert/convert_tf_checkpoint_to_pytorch.py \
+  --tf_checkpoint_path $BERT_BASE_DIR_NEW/model.ckpt-20 \
+  --bert_config_file $BERT_BASE_DIR/bert_config.json \
+  --pytorch_dump_path ./test/pytorch_model.bin
 
 # pytorch_pretrained_bert convert_tf_checkpoint_to_pytorch \
 #   $BERT_BASE_DIR/bert_model.ckpt \
