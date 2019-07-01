44c20a8ef6f5dd2a99c989b76ea78d3771934618
Jian Zhang <zhangjianthu@gmail.com>
2019-07-01 23:54:28 +0000

    experiments done for evaluation on the lr grid search ckpt

--- src/bert-pretraining/experiments.py
+++ src/bert-pretraining/experiments.py
@@ -31,6 +31,7 @@ def bert_pretraining_lr_tuning_training():
 
 def bert_pretraining_lr_tuning_evaluation():
     file_name = SCRIPT_FOLDER + "/0701_bert_pretraining_lr_tuning_eval"
+    print("cmd in ", file_name)
     lrs = [0.000001, 0.00001, 0.0001, 0.001, 0.01]
     BERT_BASE_DIR = "../../data/bert"
     tpu_tmp = 'gcloud compute tpus create tpu-{} --range=10.240.{}.0 --version=1.13 --accelerator-type=v2-8 --network=default &'
@@ -40,15 +41,15 @@ def bert_pretraining_lr_tuning_evaluation():
             --do_eval=True \
             --bert_config_file=../../data/bert/3_layer_bert_config.json \
             --eval_batch_size=256 \
-            --init_ckptpoint=gs://embeddings-ckpt/bert_pretraining_lr_tuning/pretrain_tuning_lr_{}/model.ckpt-250000 \
+            --init_checkpoint=gs://embeddings-ckpt/bert_pretraining_lr_tuning/pretrain_tuning_lr_{}/model.ckpt-250000 \
             --max_seq_length=128 \
             --max_predictions_per_seq=20 \
             --use_tpu=True \
-            --tpu_name=tpu-{} 2>&1 | tee output/pretrain_tuning_lr_{}_eval.log &')
+            --tpu_name=tpu-0 2>&1 | tee output/pretrain_tuning_lr_{}_eval.log')
     with open(file_name, 'w') as f:
-        for i, lr in enumerate(lrs):
-           cmd_str = tpu_tmp.format(i, i)
-           f.write(cmd_str + "\n")
+        #for i, lr in enumerate(lrs):
+        #   cmd_str = tpu_tmp.format(i, i)
+        #   f.write(cmd_str + "\n")
         for i, lr in enumerate(lrs):
             cmd_str = run_tmp.format(lr, lr, i, lr)
             f.write(cmd_str + "\n")
