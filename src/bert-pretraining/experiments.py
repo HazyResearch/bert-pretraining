ff8b27dbb528dd2ecf34ff681f9008b2189fc834
Jian Zhang <zhangjianthu@gmail.com>
2019-07-01 02:21:35 -0700

    add script for lr tuning training phase

---
+++ src/bert-pretraining/experiments.py
@@ -0,0 +1,29 @@
+import json
+
+SCRIPT_FOLDER="../../script"
+
+def bert_pretraining_lr_tuning_training():
+    file_name = SCRIPT_FOLDER + "/0701_bert_pretraining_lr_tuning_training"
+    lrs = [0.000001, 0.00001, 0.0001, 0.001, 0.01]
+    BERT_BASE_DIR = "../../data/bert"
+    tmp = ('python ../third_party/bert/run_pretraining.py \
+            --input_file=gs://embeddings-data2/bert-wiki/wiki17/wiki_tf_rec/part_tf_examples_*.tfrecord \
+            --output_dir=gs://embeddings-ckpt/pretrain_tuning_lr_{}  \
+            --do_train=True \
+            --do_eval=True \
+            --bert_config_file=../../data/bert/3_layer_bert_config.json \
+            --train_batch_size=256 \
+            --max_seq_length=128 \
+            --max_predictions_per_seq=20 \
+            --num_train_steps=250000 \
+            --num_warmup_steps=2500 \
+            --learning_rate={} \
+            --use_tpu=True \
+            --tpu_name=demo-tpu-{} 2>&1 | tee pretrain_tuning_lr_{}.log &')
+    with open(file_name, 'w') as f:
+        for i, lr in enumerate(lrs):
+            cmd_str = tmp.format(lr, lr, i, lr)
+            f.write(cmd_str + "\n")
+    
+if __name__ == "__main__":
+    bert_pretraining_lr_tuning_training()
