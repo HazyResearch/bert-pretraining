6d1e9745f7aa83aa49128d64f2b6f4b30d713832
Jian Zhang <zhangjianthu@gmail.com>
2019-07-01 01:14:35 +0000

    tf rec generation done

--- src/bert-pretraining/script/generate_tf_rec.sh
+++ src/bert-pretraining/script/generate_tf_rec.sh
@@ -21,23 +21,24 @@ for name in "${names[@]}"; do
 	  --max_predictions_per_seq=20 \
 	  --masked_lm_prob=0.15 \
 	  --random_seed=123 \
-	  --dupe_factor=5 &
+	  --dupe_factor=5 
  done
 
-for name in "${names[@]}"; do
-    for ((i=0;i<=9;i++)); do
-        python create_pretraining_data.py \
-              --input_file=$BASE_DIR/data/wiki/${name}/wiki_txt/wiki_bert_${i}.txt \
-              --output_file=$EMBED_DATA_DIR/bert-wiki/${name}/wiki_tf_rec/part_tf_examples_${i}.tfrecord \
-              --vocab_file=$BASE_DIR/data/bert/uncased_L-12_H-768_A-12/vocab.txt \
-              --do_lower_case=True \
-              --max_seq_length=128 \
-              --max_predictions_per_seq=20 \
-              --masked_lm_prob=0.15 \
-              --random_seed=123 \
-              --dupe_factor=5 &
-    done
-done
+# generate 10 subfiles
+#for name in "${names[@]}"; do
+#    for ((i=0;i<=9;i++)); do
+#        python create_pretraining_data.py \
+#              --input_file=$BASE_DIR/data/wiki/${name}/wiki_txt/wiki_bert_${i}.txt \
+#              --output_file=$EMBED_DATA_DIR/bert-wiki/${name}/wiki_tf_rec/part_tf_examples_${i}.tfrecord \
+#              --vocab_file=$BASE_DIR/data/bert/uncased_L-12_H-768_A-12/vocab.txt \
+#              --do_lower_case=True \
+#              --max_seq_length=128 \
+#              --max_predictions_per_seq=20 \
+#              --masked_lm_prob=0.15 \
+#              --random_seed=123 \
+#              --dupe_factor=5 &
+#    done
+#done
 
 
 popd
