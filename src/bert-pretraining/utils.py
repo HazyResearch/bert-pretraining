6793f3195e160093da9c908e701e69be3bfeff11
Jian Zhang <zhangjianthu@gmail.com>
2019-07-02 07:53:33 +0000

    add tf seed specifier for 3 seed experiments on pretraining

--- src/bert-pretraining/utils.py
+++ src/bert-pretraining/utils.py
@@ -3,6 +3,6 @@ import numpy as np
 import sys
 
 def set_tensorflow_random_seed(rand_seed):
-    random.seed(config['seed'])
-	np.random.seed(config['seed'])
-	tf.set_random_seed(config['seed'])
+    random.seed(rand_seed)
+    np.random.seed(rand_seed)
+    tf.set_random_seed(rand_seed)
