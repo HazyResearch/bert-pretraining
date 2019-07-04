9a409fbbc92cb36e3e5ea78f2f3f17e7940aaf98
Jian Zhang <zhangjianthu@gmail.com>
2019-07-04 15:55:06 -0700

    first version of feature generation script

--- src/bert-pretraining/utils.py
+++ src/bert-pretraining/utils.py
@@ -6,3 +6,9 @@ def set_tensorflow_random_seed(rand_seed):
     random.seed(rand_seed)
     np.random.seed(rand_seed)
     tf.set_random_seed(rand_seed)
+
+def get_date_str():
+    return '{:%Y-%m-%d}'.format(datetime.date.today())
+
+def ensure_dir(dir):
+    pathlib.Path(dir).mkdir(parents=True, exist_ok=True)
