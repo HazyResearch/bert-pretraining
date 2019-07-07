c7915eda593940417df9e96723d2f51128c37ccb
Jian Zhang <zhangjianthu@gmail.com>
2019-07-06 17:37:02 -0700

    test done for lstm and linear for bert sentiment

--- src/bert-pretraining/utils.py
+++ src/bert-pretraining/utils.py
@@ -1,7 +1,10 @@
 import tensorflow as tf
 import numpy as np
-import sys
+import sys, os
 import datetime
+import logging
+import pathlib
+import json
 
 def set_tensorflow_random_seed(rand_seed):
     random.seed(rand_seed)
@@ -13,3 +16,25 @@ def get_date_str():
 
 def ensure_dir(dir):
     pathlib.Path(dir).mkdir(parents=True, exist_ok=True)
+
+def save_to_json(dict_to_write, path):
+    with open(path, 'w') as f: json.dump(dict_to_write, f, indent=2)
+
+def init_logging(path):
+    """Initialize logfile to be used for experiment."""
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    
+    # create console handler and set level to info
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    # create error file handler and set level to error
+    handler = logging.FileHandler(os.path.join(path, "run.log"),"w", encoding=None, delay="true")
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
