63d131578d68e1a5992ae3781e76ab5392a8eb59
Jian Zhang <zhangjianthu@gmail.com>
2019-07-10 10:35:43 -0700

    final commit after first round bert experiments are done

--- src/bert-pretraining/plotting.py
+++ src/bert-pretraining/plotting.py
@@ -53,7 +53,24 @@ def plot_all_bert_stab_vs_ensemble():
 		plt.savefig(csv_name.replace(".csv", ".png").replace("csv", "figure"))
 		plt.close()
 
+def plot_all_bert_qual_vs_ensemble():
+	all_csv = glob.glob("./output/csv/qual_vs_ensemble*.csv")
+	for csv_name in all_csv:
+		groups, names, data = plot_utils.csv_to_table(csv_name)
+		plt.figure()
+		plot_utils.plot_figure_with_error_bar(names, data, color_list="b")
+		# replace suffix and folder for pdf
+		print(csv_name.replace(".csv", ".pdf").replace("csv", "figure"))
+		plt.title(csv_name.split("dataset_")[1].split(".")[0], fontsize=17)
+		plt.ylabel("Test err.", fontsize=17)
+		plt.xlabel("Epsilon", fontsize=17)
+		# plt.xscale("log")
+		plt.savefig(csv_name.replace(".csv", ".pdf").replace("csv", "figure"))
+		plt.savefig(csv_name.replace(".csv", ".png").replace("csv", "figure"))
+		plt.close()
+
 if __name__ == "__main__":
 	# plot_all_bert_stab_vs_dim()
 	# plot_all_bert_stab_vs_comp()
-	plot_all_bert_stab_vs_ensemble()
+	# plot_all_bert_stab_vs_ensemble()
+	plot_all_bert_qual_vs_ensemble()
